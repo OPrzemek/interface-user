f84f7036208f76b293825b98f2dcbaaf779b3c21
Przemysław Okun <przemyslaw.okun@wp.pl>
2026-02-15 20:13:43 +0100

    Updates to Scene and Instantiating method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,14 +10,14 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        InstantiateText("woohoo", new Vector2(-7,-1), .7f);
-        InstantiateText("working", new Vector2(-2,-4));
+        InstantiateText("interface", new Vector2(-4.5f,2));
+        InstantiateText("user", new Vector2(-2,0), 0.7f);
     }
 
     private void InstantiateText(string text, Vector2 position, float scale = 1)
     {
         text = text.ToUpper();
-        float positionX = position.x;
+        float positionX = position.x + 0.5f;
         List<GameObject>  letters = new List<GameObject>();
         foreach (char c in text)
         {
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
                 letters.Add(prefabToSpawn);
             }
         }
-        GameObject parent = Instantiate(new GameObject(), position + new Vector2(letters.Count / 2f - 0.5f, 0), Quaternion.identity);
+        GameObject parent = Instantiate(new GameObject(), position + new Vector2(letters.Count / 2f, 0), Quaternion.identity);
         foreach (GameObject letter in letters)
         {
             Instantiate(letter, new Vector2(positionX, position.y), Quaternion.identity, parent.transform);
