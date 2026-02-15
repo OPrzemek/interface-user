7c9c638ce9a5273c7fb4deda305ac961c8e933c0
Przemysław Okun <przemyslaw.okun@wp.pl>
2026-02-15 14:28:38 +0100

    Added Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    private BoxCollider2D _boxCollider2D;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private float moveSpeed = 5;
+    [SerializeField]
+    private float jumpVelocityIncrease = 2;
+    [SerializeField]
+    private float maxJumpVelocity = 15;
+    private float _currentJumpVelocity;
+
+    private bool _jumped;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _jumped = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move
+        _rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, _rb.linearVelocity.y);
+        // Jump
+        if (Input.GetKey(KeyCode.Space) && !_jumped)
+        {
+            if (_currentJumpVelocity < maxJumpVelocity)
+            {
+                _rb.linearVelocity += new Vector2(0, jumpVelocityIncrease);
+                _currentJumpVelocity = _rb.linearVelocity.y;
+            }
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+            _jumped = true;
+        if (IsGrounded())
+        {
+            _currentJumpVelocity = 0f;
+            _jumped = false;
+        }
+    }
+    
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size,
+            0f, Vector2.down, 0.1f, whatIsGround);
+        return hit.collider != null;
     }
 }
