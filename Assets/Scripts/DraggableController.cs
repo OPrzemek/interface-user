f448b63e180dde71268af1f71fc23077a17df569
Przemysław Okun <przemyslaw.okun@wp.pl>
2026-02-15 22:15:20 +0100

    Added slider platform and made draggable platforms have no collider when dragging

--- Assets/Scripts/DraggableController.cs
+++ Assets/Scripts/DraggableController.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class DraggableController : MonoBehaviour
 {
     private Rigidbody2D _selectedRb;
+    private Collider2D _collider;
     private Vector2 _offset;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SelectObject();
+            _collider = SelectObject();
         }
 
         if (Input.GetMouseButtonUp(0))
         {
+            if (_collider != null)
+                _collider.enabled = true;
             _selectedRb = null;
         }
     }
@@ -27,7 +30,7 @@ public class DraggableController : MonoBehaviour
         }
     }
 
-    private void SelectObject()
+    private Collider2D SelectObject()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -35,10 +38,16 @@ public class DraggableController : MonoBehaviour
 
         if (hit.collider != null)
         {
-            if (!hit.collider.CompareTag("DraggablePlatform")) return;
+            if (!hit.collider.CompareTag("DraggablePlatform"))
+                return null;
             _selectedRb = hit.collider.GetComponent<Rigidbody2D>();
             if (_selectedRb != null)
+            {
+                hit.collider.enabled = false;
                 _offset = mousePos - _selectedRb.position;
+                return hit.collider;
+            }
         }
+        return null;
     }
 }
