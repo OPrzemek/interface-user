db61699c4b53fe9e843181ee0fa6a1bb16992a31
Yukesh  J <yukeshj221106@gmail.com>
2026-02-17 09:57:06 +0530

    Cursor Select
    the buttons work only for the cursor

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Button : MonoBehaviour
 {
+    public GameObject Cursor;
     [SerializeField]
     private SceneIndex sceneIndex;
     
     private void OnCollisionEnter2D(Collision2D other)
     {
+        if(other.gameObject==Cursor)
         SceneManager.LoadScene((int)sceneIndex);
     }
 }
