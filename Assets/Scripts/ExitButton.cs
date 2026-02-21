73812f512f5e348e072510ac654922e369edbb0c
Yukesh  J <yukeshj221106@gmail.com>
2026-02-21 19:41:08 +0530

    endScreen

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -1,17 +1,20 @@
 using TMPro;
 using UnityEngine;
+using System.Collections;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class ExitButton : MonoBehaviour
 {
-    [SerializeField]
-    private TMP_Text[] exitText;
+    [SerializeField] private TMP_Text[] exitText;
+    [SerializeField] private CanvasGroup endPanel;
+    [SerializeField] private GameObject cursor;
 
-    private Button button;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool locked;
+
     void Start()
     {
-        button = GetComponent<Button>();
-        var locked = false;
+        locked = false;
+
         for (int i = 0; i < GameManager.Instance.EXIT.Length; i++)
         {
             if (GameManager.Instance.EXIT[i])
@@ -22,7 +25,34 @@ public class ExitButton : MonoBehaviour
                 locked = true;
             }
         }
+    }
 
-        button.Locked = locked;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (locked) return;
+
+        if (other.gameObject == cursor)
+        {
+            StartCoroutine(EndSequence());
+        }
+    }
+
+    IEnumerator EndSequence()
+    {
+        endPanel.blocksRaycasts = true;
+
+        float duration = 1f;
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            endPanel.alpha = timer / duration;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        Application.Quit();
     }
 }
